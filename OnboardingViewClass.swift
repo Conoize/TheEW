d2b50452c074d4e1a2bb9136e526eefa4cb4ad55
Conoize <conoize@hotmail.com>
2018-06-14 08:25:24 -0400

    Add files via upload

---
+++ OnboardingViewClass.swift
@@ -0,0 +1,16 @@
+//
+//  OnboardingViewClass.swift
+//  Side Menu
+//
+//  Created by Christian Vargas on 6/13/18.
+//  Copyright © 2018 Christian O. Vargas. All rights reserved.
+//
+
+import UIKit
+import paper_onboarding
+
+class OnboardingViewClass: PaperOnboarding {
+
+    
+    
+}
